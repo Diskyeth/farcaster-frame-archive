bd918dde8b4c4d22ad503c4dff05b30dc5ca4eaf
Diskyeth <evert@evertmartin.be>
2025-03-16 17:10:42 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ const appUrl = process.env.NEXT_PUBLIC_URL || "https://www.legacyframes.xyz";
 
 const frame = {
   version: "1.0.0",
-  imageUrl: `https://www.legacyframes.xyz/`,
+  imageUrl: `https://www.legacyframes.xyz/banner.png`,
   button: {
     title: "Launch Frame",
     action: {
       type: "launch_frame",
       name: "Legacy Frames Archive",
-      url: appUrl,
-      splashImageUrl: `https://www.legacyframes.xyz/banner.png`,
+      url: `https://www.legacyframes.xyz/`,
+      splashImageUrl: `https://www.legacyframes.xyz/icon.png`,
       splashBackgroundColor: "#10001D",
     },
   },
