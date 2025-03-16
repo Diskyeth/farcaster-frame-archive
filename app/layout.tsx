86ae10ca83e3688c885a09f74f1c3beb4ef3a080
Diskyeth <evert@evertmartin.be>
2025-03-16 17:13:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,30 @@ import './globals.css';
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: 'Legacy Frames Archive',
-  description: 'To preseve and enjoy V1 Frames',
-};
-
+  title: 'Legacy Frames',
+  description: 'Post anonymously to Farcaster.',
+  openGraph: {
+    title: 'Legacy Frames',
+    description: 'Post anonymously to Farcaster.',
+    images: ['/icon.png'],
+  },
+  other: {
+    ['fc:frame']: JSON.stringify({
+      version: 'next',
+      imageUrl: 'https://legacyframes.xyz/banner.png',
+      button: {
+        title: 'Post anonymously',
+        action: {
+          type: 'launch_frame',
+          name: 'Legacy Frames',
+          url: 'https://legacyframes.xyz/',
+          splashImageUrl: 'https://legacyframes.xyz/icon.png',
+          splashBackgroundColor: '#151515',
+        },
+      },
+    }),
+  },
+}
 export default function RootLayout({
   children,
 }: Readonly<{
